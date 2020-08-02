a086d1642229402ec8dababa91e5af0f18e45286
Roger <13369189710@163.com>
2020-08-02 21:55:56 +0900

    完善 2.54 的程序，并不是所有条件都为真

--- deeply-in-computer-system/2.54/main.c
+++ deeply-in-computer-system/2.54/main.c
@@ -4,10 +4,15 @@ int main() {
 	int x = 3; 
 	float f = 3.2;
 	double d = 34.1808329832;
+	printf("x = %d\n", x);
+	printf("f = %f\n", f);
+	printf("d = %lf\n", d);
 	printf("x == (int)(double)x -> %d\n", x == (int)(double)x);
 	printf("x == (int)(float)x -> %d\n", x == (int)(float)x);
-	printf("d == (double)(float)d -> %d\n", x == (int)(double)x);
-	printf("x == (int)(double)x -> %d\n", x == (int)(double)x);
-	printf("x == (int)(double)x -> %d\n", x == (int)(double)x);
-	printf("x == (int)(double)x -> %d\n", x == (int)(double)x);
+	printf("d == (double)(float)d -> %d\n", d == (double)(float)d);
+	printf("f == (float)(double)f -> %d\n", f == (float)(double)f);
+	printf("f == -(-f) -> %d\n", f == -(-f));
+	printf("1.0/2 == 1/2.0 -> %d\n", 1.0/2 == 1/2.0);
+	printf("d*d >= 0.0 -> %d\n", d*d >= 0.0);
+	printf("(f+d)-f == d -> %d\n", (f+d)-f == d);
 }
