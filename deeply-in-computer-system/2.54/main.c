56fb8f17330fa192329422883a1077c0297935ec
luojie <13369189710@163.com>
2020-08-02 20:43:16 +0800

    上传第一个测试程序，未完成

---
+++ deeply-in-computer-system/2.54/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int x = 3; 
+	float f = 3.2;
+	double d = 34.1808329832;
+	printf("x == (int)(double)x -> %d\n", x == (int)(double)x);
+	printf("x == (int)(float)x -> %d\n", x == (int)(float)x);
+	printf("d == (double)(float)d -> %d\n", x == (int)(double)x);
+	printf("x == (int)(double)x -> %d\n", x == (int)(double)x);
+	printf("x == (int)(double)x -> %d\n", x == (int)(double)x);
+	printf("x == (int)(double)x -> %d\n", x == (int)(double)x);
+}
